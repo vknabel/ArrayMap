babc42f967bd218a4bd2a3869307ee64d119dc6b
Valentin Knabel <develop@vknabel.com>
2016-07-30 10:41:56 +0200

    Added Support for Swift 2.3

--- ArrayMap/ArrayMap.swift
+++ ArrayMap/ArrayMap.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Conclurer GmbH. All rights reserved.
 //
 
-@warn_unused_result
+
 private func countArrayMapRawValue<Key: Hashable, Value>(rawValue: [Key: [Value]]) -> Int {
     return rawValue.reduce(0) { $0 + $1.1.count }
 }
@@ -26,17 +26,17 @@ public struct ArrayMap<Key: Hashable, Value>: SequenceType {
     public typealias Generator = ArrayMapGenerator<Key, Value>
     public typealias SubSequence = Array<Generator.Element>.SubSequence
     /// - Complexity: O(1).
-    @warn_unused_result
+    
     public func generate() -> Generator {
         return Generator(arrayMap: self)
     }
     /// - Complexity: O(N).
-    @warn_unused_result
+    
     public func underestimateCount() -> Int {
         return countArrayMapRawValue(rawValue)
     }
     /// - Complexity: O(N).
-    @warn_unused_result
+    
     public func map<T>(@noescape transform: (Generator.Element) throws -> T) rethrows -> [T] {
         var maps = [T]()
         for e in self {
@@ -45,7 +45,7 @@ public struct ArrayMap<Key: Hashable, Value>: SequenceType {
         return maps
     }
     /// - Complexity: O(N)
-    @warn_unused_result
+    
     public func filter(@noescape includeElement: (Generator.Element) throws -> Bool) rethrows -> [Generator.Element] {
         var maps: [Generator.Element] = []
         for e in self {
@@ -63,28 +63,27 @@ public struct ArrayMap<Key: Hashable, Value>: SequenceType {
     }
     /// - Requires: `n >= 0`
     /// - Complexity: O(N)
-    @warn_unused_result
+    
     public func dropFirst(n: Int = 1) -> SubSequence {
         return Array(self).dropFirst(n)
     }
     /// - Requires: `n >= 0`
     /// - Complexity: O(`self.count`)
-    @warn_unused_result
+    
     public func dropLast(n: Int = 1) -> SubSequence {
         return Array(self).dropLast(n)
     }
     /// - Requires: `maxLength >= 0`
-    @warn_unused_result
+    
     public func prefix(maxLength: Int) -> SubSequence {
         return Array(self).prefix(maxLength)
     }
     /// - Requires: `maxLength >= 0`
-    @warn_unused_result
+    
     public func suffix(maxLength: Int) -> SubSequence {
         return Array(self).suffix(maxLength)
     }
     /// - Requires: `maxSplit >= 0`
-    @warn_unused_result
     public func split(maxSplit: Int, allowEmptySlices: Bool, @noescape isSeparator: (Generator.Element) throws -> Bool) rethrows -> [SubSequence] {
         return try Array(self).split(maxSplit, allowEmptySlices: allowEmptySlices, isSeparator: isSeparator)
     }
